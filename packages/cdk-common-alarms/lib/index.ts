debad7c90a16e783434256b40a3f5675d611cb6a
campionfellin <campionfellin@gmail.com>
2020-02-13 19:46:56 -0800

    Export new alarms
    Signed-off-by: campionfellin <campionfellin@gmail.com>

--- packages/cdk-common-alarms/lib/index.ts
+++ packages/cdk-common-alarms/lib/index.ts
@@ -1,3 +1,8 @@
 import { AccountRCUAlarm } from './dynamodb/AccountRCUAlarm' 
+import { AccountWCUAlarm } from './dynamodb/AccountWCUAlarm' 
+import { TableRCUAlarm } from './dynamodb/TableRCUAlarm' 
+import { TableWCUAlarm } from './dynamodb/TableWCUAlarm' 
+import { ReadThrottlingAlarm } from './dynamodb/ReadThrottlingAlarm' 
+import { WriteThrottlingAlarm } from './dynamodb/WriteThrottlingAlarm' 
 
-export { AccountRCUAlarm }
+export { AccountRCUAlarm, AccountWCUAlarm, TableRCUAlarm, TableWCUAlarm, ReadThrottlingAlarm, WriteThrottlingAlarm }
